722594fddaf270fb00a71a3e86708bf3a9df0217
saugat330 <ssaugat349@gmail.com>
2023-02-23 10:35:58 +0545

    two dimensional array

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+                                public static void main(String[] args)
+        {
+            int [][] y = {
+                            {1,2,3},
+                        {   4,5,6}
+                        };
+
+        for (int i=0; i<y.length; i++) {
+            for (int j=0; j<y[i].length;j++) {
+                System.out.println(y[i][j]);
+
+
+            }
+        }
+        }
+    }
+
+
